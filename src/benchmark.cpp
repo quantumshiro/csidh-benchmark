e0fd346de1bbea1e97f5e84e7d640993df03f129
quantumshiro <hiro.1nakanishi@gmail.com>
2023-06-14 11:40:29 +0900

    benchmark: uint operations

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -23,8 +23,56 @@ static void bench_RandomBytes(benchmark::State& state) {
         randombytes(x, 32);
     }
 }
-
 BENCHMARK(bench_RandomBytes);
+
+static void bench_uintset(benchmark::State& state) {
+    for (auto _ : state) {
+        unit x;
+        uint_set(&x, 0);
+    }
+}
+BENCHMARK(bench_uintset);
+
+static void bench_uintbit(benchmark::State& state) {
+    unit x;
+    uint_set(&x, 0);
+    for (auto _ : state) {
+        uint_bit(&x, 0);
+    }
+}
+BENCHMARK(bench_uintbit);
+
+static void bench_uintadd3(benchmark::State& state) {
+    unit x, y, z;
+    uint_set(&x, 0);
+    uint_set(&y, 0);
+    uint_set(&z, 0);
+    for (auto _ : state) {
+        uint_add3(&x, &y, &z);
+    }
+}
+BENCHMARK(bench_uintadd3);
+
+static void bench_uintsub3(benchmark::State& state) {
+    unit x, y, z;
+    uint_set(&x, 0);
+    uint_set(&y, 0);
+    uint_set(&z, 0);
+    for (auto _ : state) {
+        uint_sub3(&x, &y, &z);
+    }
+}
+BENCHMARK(bench_uintsub3);
+
+static void bench_uintmul3_64(benchmark::State& state) {
+    unit x, y;
+    uint_set(&x, 0);
+    uint_set(&y, 0);
+    for (auto _ : state) {
+        uint_mul3_64(&x, &y, 0);
+    }
+}
+BENCHMARK(bench_uintmul3_64);
 
 
 BENCHMARK_MAIN();
