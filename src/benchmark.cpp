12d3c8775165e643c3f50b8b1bfec820d85dc70b
quantumshiro <hiro.1nakanishi@gmail.com>
2023-06-17 18:11:49 +0900

    add: montogomery operation benchmark

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -74,5 +74,163 @@ static void bench_uintmul3_64(benchmark::State& state) {
 }
 BENCHMARK(bench_uintmul3_64);
 
+// benchmark fp
+static void bench_fp_set(benchmark::State& state) {
+    fp x;
+    for (auto _ : state) {
+        fp_set(&x, 0);
+    }
+}
+BENCHMARK(bench_fp_set);
 
+static void bench_fp_add3(benchmark::State& state) {
+    fp x, y, z;
+    fp_set(&x, 0);
+    fp_set(&y, 0);
+    fp_set(&z, 0);
+    for (auto _ : state) {
+        fp_add3(&x, &y, &z);
+    }
+}
+BENCHMARK(bench_fp_add3);
+
+static void bench_fp_sub3(benchmark::State& state) {
+    fp x, y, z;
+    fp_set(&x, 0);
+    fp_set(&y, 0);
+    fp_set(&z, 0);
+    for (auto _ : state) {
+        fp_sub3(&x, &y, &z);
+    }
+}
+BENCHMARK(bench_fp_sub3);
+
+static void bench_fp_mul3(benchmark::State& state) {
+    fp x, y, z;
+    fp_set(&x, 0);
+    fp_set(&y, 0);
+    fp_set(&z, 0);
+    for (auto _ : state) {
+        fp_mul3(&x, &y, &z);
+    }
+}
+BENCHMARK(bench_fp_mul3);
+
+static void bench_fp_sq1(benchmark::State& state) {
+    fp x;
+    fp_set(&x, 0);
+    for (auto _ : state) {
+        fp_sq1(&x);
+    }
+}
+BENCHMARK(bench_fp_sq1);
+
+static void bench_fp_sq2(benchmark::State& state) {
+    fp x, y;
+    fp_set(&x, 0);
+    fp_set(&y, 0);
+    for (auto _ : state) {
+        fp_sq2(&x, &y);
+    }
+}
+BENCHMARK(bench_fp_sq2);
+
+static void bench_fp_inv(benchmark::State& state) {
+    fp x;
+    fp_set(&x, 0);
+    for (auto _ : state) {
+        fp_inv(&x);
+    }
+}
+BENCHMARK(bench_fp_inv);
+
+static void bench_fp_issquare(benchmark::State& state) {
+    fp x;
+    fp_set(&x, 0);
+    for (auto _ : state) {
+        fp_issquare(&x);
+    }
+}
+BENCHMARK(bench_fp_issquare);
+
+static void bench_fp_random(benchmark::State& state) {
+    fp x;
+    for (auto _ : state)
+    {
+        fp_random(&x);
+    }
+}
+BENCHMARK(bench_fp_random);
+
+// benchmark mont
+static void bench_mont_XDBL(benchmark::State& state) {
+    proj Q, A, P;
+    fp_random(&A.x);
+    fp_random(&A.z);
+    fp_random(&P.x);
+    fp_random(&P.z);
+    for (auto _ : state) {
+        xDBL(&Q, &A, &P);
+    }
+}
+BENCHMARK(bench_mont_XDBL);
+
+static void bench_mont_XADD(benchmark::State& state) {
+    proj S, P, Q, PQ;
+    fp_random(&S.x);
+    fp_random(&S.z);
+    fp_random(&P.x);
+    fp_random(&P.z);
+    fp_random(&Q.x);
+    fp_random(&Q.z);
+    for (auto _ : state) {
+        xADD(&PQ, &S, &P, &Q);
+    }
+}
+BENCHMARK(bench_mont_XADD);
+
+static void bench_mont_XDBLADD(benchmark::State& state) {
+    proj S, P, Q, PQ, A, B;
+    fp_random(&S.x);
+    fp_random(&S.z);
+    fp_random(&P.x);
+    fp_random(&P.z);
+    fp_random(&Q.x);
+    fp_random(&Q.z);
+    fp_random(&A.x);
+    fp_random(&A.z);
+    fp_random(&B.x);
+    fp_random(&B.z);
+    for (auto _ : state) {
+        xDBLADD(&PQ, &S, &P, &Q, &A, &B);
+    }
+}
+BENCHMARK(bench_mont_XDBLADD);
+
+static void bench_mont_XMUL(benchmark::State& state) {
+    proj a, b, c;
+    unit d;
+    fp_random(&a.x);
+    fp_random(&a.z);
+    fp_random(&b.x);
+    fp_random(&b.z);
+    for (auto _ : state) {
+        xMUL(&c, &a, &b, &d);
+    }
+}
+BENCHMARK(bench_mont_XMUL);
+
+static void bench_mont_ISOG(benchmark::State& state) {
+    proj P, Q, R;
+    uint64_t k = 3;
+    fp_random(&P.x);
+    fp_random(&P.z);
+    fp_random(&Q.x);
+    fp_random(&Q.z);
+
+    for (auto _ : state) {
+        xISOG(&R, &P, &Q, k);
+    }
+}
+BENCHMARK(bench_mont_ISOG);
 BENCHMARK_MAIN();
