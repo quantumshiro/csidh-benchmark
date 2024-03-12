5aeac55e6a5ba04c4dca45ce25b9c8738f4715bd
quantumshiro <hiro.1nakanishi@gmail.com>
2024-03-13 03:07:53 +0900

    add: classgroup struct and initiarize

--- src/classgroup.zig
+++ src/classgroup.zig
@@ -2,9 +2,24 @@ const std = @import("std");
 const bigInt = std.math.big.int;
 const HKZbasis = @import("HKZbasis.zig");
 
-pub var cn: bigInt = undefined;
-pub var half_cn: bigInt = undefined;
-pub var babai_Ainv_row: [74]bigInt = undefined;
+pub const classgroup = struct {
+    cn: bigInt,
+    half_cn: bigInt,
+    twopow: bigInt,
+    babai_Ainv_row: [74]bigInt,
+    IP: [74]bigInt,
+    B: [74]bigInt,
+    A: [74 * 74]bigInt,
+    allocator: std.mem.Allocator,
 
-pub var IP: [74]bigInt = undefined;
-pub var B: [74]bigInt = undefined;
+    const Self = @This();
+
+    pub fn init(self: Self) !void {
+        try self.cn.Managed.init(self.allocator);
+        try self.twopow.Managed.init(self.allocator);
+
+        for (self.babai_Ainv_row) |i| {
+            try self.babai_Ainv_row[i].Managed.init(self.allocator);
+        }
+    }
+};
